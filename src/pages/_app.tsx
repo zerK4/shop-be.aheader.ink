91af385422dd03e20ab6bccf09a5b4bd145eed2e
zerK4 <sebastian.v.pavel@gmail.com>
2023-04-29 10:23:15 +0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,15 +15,9 @@ export type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: any) {
   const getLayout = Component.getLayout ?? ((page: any) => page);
 
-  if (process.env.NODE_ENV === 'development') {
-    return (
-      <div className="text-sm">
-        <UserProvider>{getLayout(<Component {...pageProps} />)}</UserProvider>
-      </div>
-    );
-  }
-
   return (
-    <div className="text-sm">{getLayout(<Component {...pageProps} />)}</div>
+    <div className="text-sm">
+      <UserProvider>{getLayout(<Component {...pageProps} />)}</UserProvider>
+    </div>
   );
 }
