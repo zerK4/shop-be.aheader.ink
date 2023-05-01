1ca3acf35d292ba77579f463d4383312d73a38b6
zerK4 <sebastian.v.pavel@gmail.com>
2023-05-01 07:55:02 +0300

    .

--- src/pages/catalog/products/index.tsx
+++ src/pages/catalog/products/index.tsx
@@ -1,14 +1,12 @@
 import { renderPageHeader } from '@/components/PageHeader/PageHeader.component';
 import TableComponent from '@/components/Table/Table.component';
 import MainLayout from '@/layout/main.layout';
-import globalStore from '@/store/globalStore';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import React, { ReactElement, useEffect, useState } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 
 function Products({ data }: any) {
-  const { newProductActive } = globalStore();
   const [products, setProducts] = useState([]);
   const router = useRouter();
 
